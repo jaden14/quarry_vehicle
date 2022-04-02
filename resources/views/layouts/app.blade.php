29b5d33122cebc0a19858544790b0b80ab5a046a
GianCarloMarquez <gmarquez_180000001353@uic.edu.ph>
2022-04-02 22:26:32 +0800

    Minor edit in Page Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <title>Portal</title>
+  <title>QVV System</title>
 
     <!-- Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
+    <meta name="description" content="Quarry and Vehicle Violations System">
     <meta name="author" content="Xiaoying Riley at 3rd Wave Media">
     <link rel="shortcut icon" href="favicon.ico">
 
