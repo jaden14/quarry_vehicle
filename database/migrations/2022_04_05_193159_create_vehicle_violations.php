834812ed44a733d59779e28cbade8a081de08155
GianCarloMarquez <gmarquez_180000001353@uic.edu.ph>
2022-04-06 03:58:10 +0800

    created migration

---
+++ database/migrations/2022_04_05_193159_create_vehicle_violations.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehicleViolations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_violations', function (Blueprint $table) {
+            $table->id();
+            $table->date('date')->nullable();
+            $table->time('time')->nullable();
+            $table->string('plate_no')->nullable();
+            $table->string('responsible')->nullable();
+            $table->string('coveyance_type')->nullable();
+            $table->string('violation_type')->nullable();
+            $table->string('remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_violations');
+    }
+}
