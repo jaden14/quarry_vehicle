2b4aff1af42548c9460e02ff1c9bd9beba123834
GianCarloMarquez <gmarquez_180000001353@uic.edu.ph>
2022-04-06 16:09:06 +0800

    adding modal

--- resources/views/VehicleViolations/index.blade.php
+++ resources/views/VehicleViolations/index.blade.php
@@ -1,117 +1,79 @@
 @extends('layouts.app')
+
 @section('content')
-<div class="container-xl"> 
-    <h1 class="app-page-title">Vehicle Violations</h1>
-    <div class="app-card shadow-sm mb-4 border-left-decoration">
-        <div class="inner">
-            <div class="app-card-body p-4">
-                <div class="row gx-5 gy-3">
-                    <div class="col-12 col-lg-10">
-                        <form>
-                        <div class="form-row">
-                        <div class="form-group col-md-3">
-                            <label for="date">Date</label>
-                            <input type="date" class="form-control" id="Date">
-                        </div>
+<div class="container-xl">
 
-                        <div class="form-group col-md-3">
-                            <label for="time">Time</label>
-                            <input type="time" class="form-control" id="time">
-                        </div>
+    <div class="row g-3 mb-4 align-items-center justify-content-between">
+        <div class="col-auto">
+            <h1 class="app-page-title mb-0">Vehicle Violations</h1>
+        </div>
+        <div class="col-auto">
+            <div class="page-utilities">
+                <div class="col-auto">
 
-                        <div class="form-group col-md-3">
-                            <label for="plateno">Plate No.</label>
-                            <input type="text" class="form-control" id="plateno" placeholder="Plate Number">
-                        </div>
+                    <button type="button" class="btn app-btn-primary" data-toggle="modal" data-target=".vehicleviolations-modal-lg">Add</button>
 
-                        <div class="form-group col-md-3">
-                            <label for="responsible">Responsible</label>
-                            <input type="text" class="form-control" id="responsible" placeholder="Responsible">
-                        </div>
+                </div><!--//col-->
+            </div><!--//table-utilities-->
+        </div><!--//col-auto-->
+    </div><!--//row-->
 
-                        <div class="form-group col-md-6">
-                            <label for="Conveyance">Conveyance Type</label>
-                            <select class="form-select col-md-12">
-                                <option selected="" value="option-1">All</option>
-                                <option value="option-2">This week</option>
-                                <option value="option-3">This month</option>
-                                <option value="option-4">Last 3 months</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group col-md-6">
-                            <label for="inputAddress2">Violation</label>
-                            <select class="form-select col-md-12">
-                                <option selected="" value="option-1">All</option>
-                                <option value="option-2">This week</option>
-                                <option value="option-3">This month</option>
-                                <option value="option-4">Last 3 months</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group col-md-12">
-                            <label for="inputAddress2">Remarks</label>
-                            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                        </div>
-
-                        </div>
-                        </form><!--//end-form-->
-
-                        <table class="table app-table-hover mb-0 text-left table-bordered">
-                            <thead>
-                                <tr>
-                                    <th class="cell">ID</th>
-                                    <th class="cell">Description</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Sample Data 1</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Sample Data 2</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="col-12 col-lg-2">
-                        <table>
-                        <tr>
-                        <td>
-                        <a class="btn app-btn-primary" href="https://www.chartjs.org/docs/latest/" target="_blank"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up-right-square me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-                        <path fill-rule="evenodd" d="M5.172 10.828a.5.5 0 0 0 .707 0l4.096-4.096V9.5a.5.5 0 1 0 1 0V5.525a.5.5 0 0 0-.5-.5H6.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"></path>
-                        </svg>Add</a>
-                        <button data-swal-toast-template="#my-template">
-                        Trigger toast
+    <!-- Create Violatin Type Modal -->
+    <div class="modal fade vehicleviolations-modal-lg" tabindex="-1" role="dialog" aria-labelledby="addVehicleViolations" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-centered">
+            <div class="modal-content">
+            <form action="{{ route('vehicleviolations.store') }}" method="POST">
+                    @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="vehicleviolation-text">Vehicle Violations</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
                         </button>
-                        </td>
-                        </tr>
-                        <tr>
-                        <td>
-                        <a class="btn app-btn-secondary" href="https://www.chartjs.org/docs/latest/" target="_blank"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up-right-square me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-                        <path fill-rule="evenodd" d="M5.172 10.828a.5.5 0 0 0 .707 0l4.096-4.096V9.5a.5.5 0 1 0 1 0V5.525a.5.5 0 0 0-.5-.5H6.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"></path>
-                        </svg>Edit</a>
-                        </td>
-                        </tr>
-                        <tr>
-                        <td>
-                        <a class="btn app-btn-secondary" href="https://www.chartjs.org/docs/latest/" target="_blank"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up-right-square me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-                        <path fill-rule="evenodd" d="M5.172 10.828a.5.5 0 0 0 .707 0l4.096-4.096V9.5a.5.5 0 1 0 1 0V5.525a.5.5 0 0 0-.5-.5H6.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"></path>
-                        </svg>Delete</a>
-                        </td>
-                        </tr>
-                        </table>   
-                    </div><!--//col-->
-                </div><!--//row-->
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-row">
+                            <div class="form-group col-md-3">
+                                <label for="date">Date</label>
+                                <input type="date" class="form-control" id="Date">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label for="time">Time</label>
+                                <input type="time" class="form-control" id="time">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label for="plateno">Plate No.</label>
+                                <input type="text" class="form-control" id="plateno" placeholder="Plate Number">
+                            </div>
+                            <div class="form-group col-md-3">
+                                <label for="responsible">Responsible</label>
+                                <input type="text" class="form-control" id="responsible" placeholder="Responsible">
+                            </div>
 
-            </div><!--//app-card-body-->
-            
-        </div><!--//inner-->
-    </div><!--//app-card-->
+                            <div class="form-group col-md-6">
+                                <label for="Conveyance">Conveyance Type</label>
+                                <select class="form-select col-md-12">
+                                    <option selected="" value="option-1">Select here</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputAddress2">Violation</label>
+                                <select class="form-select col-md-12">
+                                    <<option selected="" value="option-1">Select here</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="inputAddress2">Remarks</label>
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Remarks">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn app-btn-secondary">Save</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+            </form>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
